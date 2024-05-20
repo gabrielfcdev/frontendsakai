7b76fdcc0455856b0be63715520ae08b1aa0c821
Gabriel Calixto <gfcdev@outlook.com.br>
2024-05-20 11:20:21 -0300

    Insert

---
+++ types/projeto.d.ts
@@ -0,0 +1,12 @@
+declare namespace Projeto {
+
+    type Usuario = {
+        id?: number;
+        nome: string;
+        login: string;
+        senha: string;
+        email: string;
+
+        
+    };
+}
